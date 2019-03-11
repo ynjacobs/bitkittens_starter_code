45e520ef793f7908e8f95ef9ea205a5aa8c3cdb5
NatalieBlack <natalievictoriablack@gmail.com>
2019-03-11 17:07:03 -0400

    Starter code

--- bitkittens/settings.py
+++ bitkittens/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bitkittens',
 ]
 
 MIDDLEWARE = [
@@ -74,10 +75,6 @@ WSGI_APPLICATION = 'bitkittens.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
 }
 
 
