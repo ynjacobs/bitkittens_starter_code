1680fa13db38e2aa813422589e60f0a0e7971139
ynjacobs <ytj64611@gamil.com>
2019-06-20 16:30:09 -0400

    takes more than 3 cats and puts in specified number of divs

--- bitkittens/static/js/cats.js
+++ bitkittens/static/js/cats.js
@@ -2,12 +2,57 @@ document.addEventListener("DOMContentLoaded", function() {
     let summonBtn = document.querySelector('.summon-cats');
     summonBtn.addEventListener('click', function(event){
         console.log('code works');
-        let catDivs = document.querySelectorAll('.cat-box');
+        let pageMain = document.querySelector('main');
 
-        axios.get('http://bitkittens.herokuapp.com/cats.json')
+
+        // axios.get('http://bitkittens.herokuapp.com/cats.json')
+        //     .then(response => {
+        //         let cats = response.data.cats;
+        //         console.log(cats);
+        //         cats.forEach(function(cat, index){
+        //             let name = cat.name;
+        //             let imgSrc = cat.photo;
+        //             let imgAlt = `Photo of ${name}`;
+        //             let image = document.createElement('img');
+        //             image.src = imgSrc;
+        //             image.alt = imgAlt;
+        //             catDivs[index].innerHTML = '';
+        //             catDivs[index].appendChild(image);
+
+
+
+        //         });
+
+        //     })
+        
+        axios.get('http://bitkittens.herokuapp.com/cats.json?number=5')
             .then(response => {
                 let cats = response.data.cats;
+
                 console.log(cats);
+                if(cats.length > 3)
+                {
+                    let x = '200px';
+                    let y = '100px';
+                    for(var i=3; i < cats.length; i++)
+                    {
+                        let aDiv = document.createElement('div');
+                        aDiv.classList.add('cat-box');
+                        aDiv.style.display = 'absolute';
+                        aDiv.style.left = x;
+                        aDiv.style.top = y;
+                        x = (parseInt(x, 10) + 50) + 'px';
+                        y = (parseInt(y, 10) + 300) + 'px';
+
+                        console.log({x});
+                        // x += 20;
+                        // y += 20;
+                        pageMain.append(aDiv);
+
+                    }
+                }
+        let catDivs = document.querySelectorAll('.cat-box');
+
                 cats.forEach(function(cat, index){
                     let name = cat.name;
                     let imgSrc = cat.photo;
